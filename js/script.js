c0dc357c00cde90c1d589b3d325e9d7aadbfe232
sathya <sathya.somu@a-cti.com>
2015-12-22 18:49:59 +0530

    final

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	 $(function(){
+        $('#box').waterfall({    
+        	// top offset
+top : false, 
+w : false, 
+col : false, 
+gap : 10,
+gridWidth : [350,700,1050],
+refresh: 500,
+timer : false,
+scrollbottom : false
+        })
+    })
+
+  });
